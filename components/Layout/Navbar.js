4fe091ce5a9332f35b5a65b90d3c356b86d22704
Philani Ncube <ncbphi001@gmail.com>
2021-11-29 20:58:55 +0200

    navbar logo

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -41,7 +41,7 @@ export default function Navbar() {
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                     alt="Workflow"
                   />
                 </div>
